8de87214d5a55ac7ec876b153f1d1747c0cb6e4d
Jerry Krinock <jerry@sheepsystems.com>
2014-01-12 21:41:06 -0800

    Found a couple dozen files which were in my BkmkMgrs project directory that should have been in my reusable code folders.  Very tedious to move them to proper locations, then re-pointing in Xcode.  I hope I didn’t screw anything up.  Also looks like a couple checkboxes for new files which I neglected to switch on in previous commit.

---
+++ SSYFirefoxCTypes-Tester-Main.m
@@ -0,0 +1,45 @@
+#import "SSYFirefoxCTypes.h"
+
+/* 
+ Mozilla's Xulrunner dylibs are apparently compiled with the magic  @executable_path
+ in their INSTALL_PATH.  A Firefox Add-On linking to the Xulrunner dylibs works when
+ run in Firefox because the Xulrunner dylibs are packaged in
+ /Applications/Firefox.app/Contents/MacOS/, alongside the executable, firefox-bin.  
+ 
+ So, because this tool is linked to these Xulrunner dylibs, you need these Xulrunner
+ dylibs to be available at the same relative path as they are in the Firefox package,
+ which is alongside the executable produced.  The dylibs needed are:
+ 
+ libmozalloc.dylib
+ libmozsqlite3.dylib
+ libnspr4.dylib
+ libnss3.dylib
+ libnssutil3.dylib
+ libplc4.dylib
+ libplds4.dylib
+ libsmime3.dylib
+ libssl3.dylib
+ libxpcom.dylib
+ 
+ You also need this executable.  I don't know why:
+ XUL
+ 
+ The above files are all available in any xulrunner SDK.
+ Some are in the bin/ subdirectory, some are in lib/.
+ */
+int main(int argc, const char *argv[]) {
+	NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init] ;
+
+	int32_t sum = SSYC_demo_int(22,33) ;
+	NSLog(@"dylib returned sum %ld", (long)sum) ;
+	
+	char* s = "Hello Firefox" ;
+	const char* uppedS = SSYC_demo_string(s) ;
+	NSLog(@"dylib returned upped '%s' to '%s'", s, uppedS) ;
+	
+	SSYC_startServer("My-Server") ;
+	
+	[pool drain] ;
+	
+	return 0 ;
+}
