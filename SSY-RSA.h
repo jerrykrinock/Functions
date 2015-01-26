0950a5604e815b2e083d4d0f7190015fc722042a
Jerry Krinock <jerry@sheepsystems.com>
2015-01-26 12:06:33 -0800

    Added detail to SSY-RSA HeaderDocs.

--- SSY-RSA.h
+++ SSY-RSA.h
@@ -18,9 +18,9 @@ typedef enum : NSUInteger {
  @brief    Verifies an RSA / ASN.1 / SHA-1 signature
  
  @details  Determines whether or not a given signature was produced by
- producing a SHA-1 digest of a given plain-text data, prepending a ASN.1
- header to it, and signing the result with an RSA private key whose public key
- is also given.
+ producing a SHA-1 digest of a given plain-text data, prepending a ASN.1 header
+ to it per Section 9.2 of RFC 3447, and signing the result with an RSA private
+ key whose public key is also given.
  
  @param    plainData  Typically you derive this from a string using
  -[NSString dataUsingEncoding:].
@@ -34,6 +34,9 @@ typedef enum : NSUInteger {
  
  This function uses Apple's Security Transform API, and therefore requires OS X
  10.7 or later.  Does not work in iOS.  Does not use openssl.
+ 
+ This code is based on Apple's CryptoCompatibility sample code, which may be
+ helpful in forking this code for similar purposes.
  */
 SSYVerifyRSAResult SSYVerifyRSA(
                                 NSData* plainData,
