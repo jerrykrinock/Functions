c4b11a6df5004b0f967c1f4b08a1903ff08c0baa
Jerry Krinock <jerry@sheepsystems.com>
2015-01-26 11:14:34 -0800

    •  My "version 2" license validation works now (using TEST licensing scripts), with the addition of SSY-RSA.h, .m.

---
+++ SSY-RSA.h
@@ -0,0 +1,45 @@
+#ifndef SSYRSA_h
+#define SSYRSA_h
+
+typedef enum : NSUInteger {
+    SSYVerifyRSAResultInProgress = 0,
+    SSYVerifyRSAResultYippeeVerified = 1,
+    SSYVerifyRSAResultSorryDidNotVerify = 2,
+    SSYVerifyRSAResultTransformReturnedWrongType,
+    SSYVerifyRSAResultCouldNotExecuteTransform,
+    SSYVerifyRSAResultCouldNotSetInputType,
+    SSYVerifyRSAResultCouldNotSetDigestType,
+    SSYVerifyRSAResultCouldNotCreateTransform,
+    SSYVerifyRSAResultCouldNotExtractPublicKey,
+    SSYVerifyRSAResultCouldNotImportPublicKey,
+} SSYVerifyRSAResult ;
+
+/*!
+ @brief    Verifies an RSA / ASN.1 / SHA-1 signature
+ 
+ @details  Determines whether or not a given signature was produced by
+ producing a SHA-1 digest of a given plain-text data, prepending a ASN.1
+ header to it, and signing the result with an RSA private key whose public key
+ is also given.
+ 
+ @param    plainData  Typically you derive this from a string using
+ -[NSString dataUsingEncoding:].
+ 
+ @param    publicKey  Typically you derive this from a string that of the form
+ -----BEGIN PUBLIC KEY-----
+ MIIBIjANB...
+ ...AQAB
+ -----END PUBLIC KEY-----
+ using -[NSString dataUsingEncoding:].
+ 
+ This function uses Apple's Security Transform API, and therefore equires OS X
+ 10.7 or later.  Does not work in iOS.  Does not use openssl.
+ */
+SSYVerifyRSAResult SSYVerifyRSA(
+                                NSData* plainData,
+                                NSData* signature,
+                                NSData* publicKey,
+                                NSError** error_p
+                                ) ;
+
+#endif
---
+++ SSY-RSA.m
@@ -0,0 +1,139 @@
+#import <Foundation/Foundation.h>
+#import "SSY-RSA.h"
+
+SSYVerifyRSAResult SSYVerifyRSA(
+                                NSData* plainData,
+                                NSData* signature,
+                                NSData* publicKey,
+                                NSError** error_p
+                                ) {
+    SSYVerifyRSAResult  result = SSYVerifyRSAResultInProgress ;
+    CFErrorRef          errorCF = NULL ;
+    
+    CFArrayRef keyItems ;
+    OSStatus err = SecItemImport(
+                                 (__bridge CFDataRef)publicKey,
+                                 NULL,
+                                 NULL,
+                                 NULL,
+                                 0,
+                                 NULL,
+                                 NULL,
+                                 &keyItems
+                                 ) ;
+    if (err != noErr) {
+        result = SSYVerifyRSAResultCouldNotImportPublicKey ;
+    }
+    
+    SecKeyRef publicSecKeyRef = NULL ;
+    if (result == SSYVerifyRSAResultInProgress) {
+        publicSecKeyRef = (SecKeyRef)[(NSArray*)keyItems firstObject] ;
+        if (publicSecKeyRef == NULL) {
+            result = SSYVerifyRSAResultCouldNotExtractPublicKey ;
+        }
+    }
+    
+    SecTransformRef     transform = NULL ;
+    if (result == SSYVerifyRSAResultInProgress) {
+        transform = SecVerifyTransformCreate(
+                                             publicSecKeyRef,
+                                             (__bridge CFDataRef)signature,
+                                             &errorCF) ;
+        if (transform == NULL) {
+            result = SSYVerifyRSAResultCouldNotCreateTransform ;
+        }
+    }
+    
+    if (result == SSYVerifyRSAResultInProgress) {
+        if (!SecTransformSetAttribute(
+                                      transform,
+                                      kSecDigestTypeAttribute,
+                                      kSecDigestSHA1,
+                                      &errorCF
+                                      )) {
+            result = SSYVerifyRSAResultCouldNotSetDigestType ;
+        }
+    }
+    
+    // Note: kSecInputIsAttributeName defaults to kSecInputIsPlainText, which
+    // is what we want in this case.  So we skip setting that one.
+    
+    if (result == SSYVerifyRSAResultInProgress) {
+        if (!SecTransformSetAttribute(
+                                      transform,
+                                      kSecTransformInputAttributeName,
+                                      (__bridge CFDataRef) plainData,
+                                      &errorCF
+                                      )) {
+            result = SSYVerifyRSAResultCouldNotSetInputType ;
+        }
+    }
+    
+    CFBooleanRef        transformResult = NULL ;
+    if (result == SSYVerifyRSAResultInProgress) {
+        transformResult = SecTransformExecute(transform, &errorCF) ;
+        if (transformResult == NULL) {
+            result = SSYVerifyRSAResultCouldNotExecuteTransform ;
+        }
+    }
+    
+    if (result == SSYVerifyRSAResultInProgress) {
+        if (CFGetTypeID(transformResult) != CFBooleanGetTypeID()) {
+            result = SSYVerifyRSAResultTransformReturnedWrongType ;
+        }
+    }
+    
+    if (result == SSYVerifyRSAResultInProgress) {
+        result = CFBooleanGetValue(transformResult)
+        ? SSYVerifyRSAResultYippeeVerified
+        : SSYVerifyRSAResultSorryDidNotVerify ;
+    }
+    
+    if (errorCF && error_p) {
+        *error_p = (NSError*)errorCF ;
+#if NO_ARC
+        [[*error_p retain] autorelease] ;
+#endif
+    }
+    
+#if 0
+#if DEBUG
+#warning Logging SSYRSAVerify()
+
+    NSString* string ;
+
+    string = [[NSString alloc] initWithData:plainData
+                                   encoding:NSASCIIStringEncoding] ;
+    NSLog(@"SSYRSAVerify: plainData:\n%@", string) ;
+#if NO_ARC
+    [string release] ;
+#endif
+
+    NSLog(@"SSYRSAVerify: signature (%ld bytes)\n%@",
+          (long)[signature length],
+          signature) ;
+    
+    string = [[NSString alloc] initWithData:publicKey
+                                   encoding:NSASCIIStringEncoding] ;
+    NSLog(@"SSYRSAVerify: publicKey:\n%@", string) ;
+#if NO_ARC
+    [string release] ;
+#endif
+    
+    NSLog(@"SSYRSAVerify: result=%ld  Error: %@", result, (NSError*)errorCF) ;
+    
+#endif
+#endif
+    
+    if (transformResult != NULL) {
+        CFRelease(transformResult) ;
+    }
+    if (errorCF != NULL) {
+        CFRelease(errorCF) ;
+    }
+    if (transform != NULL) {
+        CFRelease(transform) ;
+    }
+    
+    return result ;
+}
